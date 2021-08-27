5d5745f308bb4040dadc6923f6e217eb7040415e
redaktor.1204@gmail.com <tuastro3>
2021-08-27 17:17:49 +0300

    first commit

---
+++ src/Les3a.java
@@ -0,0 +1,7 @@
+public class Les3a {
+
+    public static void main(String[] args) {
+        System.out.println("lll333aaa");
+    }
+
+}
---
+++ src/Les3b.java
@@ -0,0 +1,7 @@
+public class Les3b {
+
+    public static void main(String[] args) {
+        System.out.println("46436");
+    }
+
+}
