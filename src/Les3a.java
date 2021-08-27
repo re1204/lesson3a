dd46e44c72de19dd2cccf081b2a87395664f9966
redaktor.1204@gmail.com <tuastro3>
2021-08-27 17:52:06 +0300

    comm22

--- src/Les3a.java
+++ src/Les3a.java
@@ -2,6 +2,8 @@ public class Les3a {
 
     public static void main(String[] args) {
         System.out.println("lll333aaa");
+        System.out.println("lll333aaa");
+        System.out.println("lll333aaa");
 
     }
 
