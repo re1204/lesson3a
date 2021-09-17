3d362239b79266ab050c6c82458df74fd4d84e90
redaktor.1204@gmail.com <redaktor.1204@gmail.com>
2021-09-17 13:56:50 +0300

    xcvxcv

--- src/Les3b.java
+++ src/Les3b.java
@@ -3,6 +3,13 @@ public class Les3b {
     public static void main(String[] args) {
         System.out.println("46436");
         System.out.println("46436");
+
+        int[] arrey = {15, 10, 10};
+        System.out.println(arrey[0]);
+        System.out.println(arrey[1]);
+        System.out.println(arrey[2]);
+
+
     }
 
 }
